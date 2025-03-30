7f5e1b2365e0fed7bc9e6e464865830058187efe
Priyanshu Ranjan <priyanshuranjan@Priyanshus-MacBook-Air.local>
2025-03-30 14:45:39 +0530

    ini express app

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express"
+const app = express()
+const port = 3002
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+// 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
